1f7decd74c8e4bd5e8d2f9ee387e4fcd3f860f58
mehmet kose <mehmetkose122@gmail.com>
2016-01-30 16:52:58 +0200

    update statistics

--- async_crawler.py
+++ async_crawler.py
@@ -16,8 +16,7 @@ from urllib.parse import urljoin, urldefrag
 
 root_url = "http://python.org"
 crawled_urls = []
-will_be_crawl_urls = [root_url, root_url.replace('http:','https:'), 
-        "%s/robots.txt" % (root_url), "%s/sitemap.xml" % (root_url)]
+url_hub = [root_url, "%s/robots.txt" % (root_url), "%s/sitemap.xml" % (root_url)]
 
 async def get_body(client, url):
     async with client.get(url) as response:
@@ -41,11 +40,13 @@ def get_links(html):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     client = aiohttp.ClientSession(loop=loop)
-    for to_crawl in will_be_crawl_urls:
+    for to_crawl in url_hub:
         raw_html = loop.run_until_complete(get_body(client, to_crawl))
         for link in get_links(raw_html):
-            if root_url in link:
-                will_be_crawl_urls.append(link)
-        print(len(will_be_crawl_urls))
+            if root_url in link and not link in crawled_urls:
+                url_hub.append(link)
+        url_hub.remove(to_crawl)
+        crawled_urls.append(to_crawl)
+        print("url hub: %s | crawled: %s  |url : %s" % (len(url_hub), len(crawled_urls), to_crawl))
     client.close()
 
