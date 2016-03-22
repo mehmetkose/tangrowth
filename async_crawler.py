fcdb3de7465467e4ea4a50faec1c39ec9a26e15c
mehmet kose <mehmetkose122@gmail.com>
2016-03-22 09:42:14 +0200

    set main function

--- async_crawler.py
+++ async_crawler.py
@@ -13,8 +13,7 @@ import asyncio
 from urllib.parse import urljoin, urldefrag
 
 root_url = "http://python.org"
-crawled_urls = []
-url_hub = [root_url, "%s/robots.txt" % (root_url), "%s/sitemap.xml" % (root_url)]
+crawled_urls, url_hub = [], [root_url, "%s/sitemap.xml" % (root_url)]
 
 async def get_body(url):
     response = await aiohttp.request('GET', url)
@@ -28,16 +27,16 @@ def get_links(html):
     new_urls = [link.split('"')[0] for link in str(html).replace("'",'"').split('href="')[1:]]
     return [urljoin(root_url, remove_fragment(new_url)) for new_url in new_urls]
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    client = aiohttp.ClientSession(loop=loop)
+async def main():
     for to_crawl in url_hub:
-        raw_html = loop.run_until_complete(get_body(to_crawl))
+        raw_html = await get_body(to_crawl)
         for link in get_links(raw_html):
             if root_url in link and not link in crawled_urls:
                 url_hub.append(link)
         url_hub.remove(to_crawl)
         crawled_urls.append(to_crawl)
         print("url hub: %s | crawled: %s  |url : %s" % (len(url_hub), len(crawled_urls), to_crawl))
-    client.close()
 
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
